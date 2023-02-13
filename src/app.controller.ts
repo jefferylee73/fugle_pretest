7ab4a446c04ee93fdde546b469715e9d0fd2efbd
Jeffery Lee <jeffery.l@evolutivelabs.com>
2023-02-13 16:57:28 +0800

    part1: Http Api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Res,
+  Query,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import axios, { AxiosRequestConfig } from 'axios';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +17,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/data')
+  async getTopStory(@Query('user') user: number, @Res() res: Response) {
+    const config: AxiosRequestConfig = {
+      url: 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty',
+      method: 'get',
+    };
+    try {
+      const { data } = await axios(config);
+      res.status(HttpStatus.OK).json({ result: data });
+    } catch (err) {
+      throw new HttpException('拿取資料失敗', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
